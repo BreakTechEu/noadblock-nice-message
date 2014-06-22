fbe2ee9032e777e28453d6b36b225196068da75b
Pedro Ventura <pedrojventura@gmail.com>
2014-06-22 23:21:31 +0200

    inital commit. Main structure and code of the plugin

--- noadblock.php
+++ noadblock.php
@@ -1,19 +1,26 @@
 <?php
 /*
 Plugin Name: NoAdblock Nice Message
-Plugin URI: https://www.pedroventura.com/wordpress/plugin2
-Description: This plugin will shows a friendly message to the user to disable adblock for the current blog.
-Version: 0.0.1
+Plugin URI: http://pedroventura.es/TjVFZ6
+Description: This plugin shows a friendly message to the users with any adblock extension on their browser. The plugin just suggests to disable the adblock for your blog with a flyout box.
+Version: 1.0.0
 Author: Pedro Ventura
 Author URI: https://www.pedroventura.com
 License: GPL2
 */
 
-add_action( 'plugins_loaded', 'noadblock_load_textdomain' );
-add_action( 'wp_enqueue_scripts', 'load_block_adblock_js' );
-add_action( 'wp_footer', 'iniciar_app_cookie' );
+add_action( 'plugins_loaded', 'load_translations' );
+add_action( 'wp_enqueue_scripts', 'load_assets' );
+add_action( 'wp_footer', 'start_plugin' );
 
-function load_block_adblock_js() {
+/**
+ * load_assets
+ * 
+ * @access public
+ *
+ * @return mixed Value.
+ */
+function load_assets() {
 	wp_enqueue_script( 'jquery');
 	wp_enqueue_script( 'nice-ads', plugins_url( '/assets/js/ads.js' , __FILE__ ) );
 	wp_enqueue_script( 'nice-message', plugins_url( '/assets/js/nice.message.js' , __FILE__ ) );
@@ -21,11 +28,25 @@ function load_block_adblock_js() {
 	wp_enqueue_style( 'css-nice-message', plugins_url( '/assets/css/nice.message.css' , __FILE__ ) );
 }
 
-function noadblock_load_textdomain() {
+/**
+ * load_translations
+ * 
+ * @access public
+ *
+ * @return mixed Value.
+ */
+function load_translations() {
 	load_plugin_textdomain('noadblock', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 
-function iniciar_app_cookie() {
+/**
+ * start_plugin
+ * 
+ * @access public
+ *
+ * @return mixed Value.
+ */
+function start_plugin() {
 	$title =  __('Adblock is actived!', 'noadblock');
 	$message =  __('This blog helps you to solve your doubts and keeps you informed. <br />Please,<b> consider to disable the adblock <u>in this site</u></b>!<br /> Thank You!', 'noadblock');
 	?>
